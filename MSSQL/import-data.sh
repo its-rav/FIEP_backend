6bd2ff2cbe77c3fd6b0dee7f6f84aa52b8f2bdc1
Nhân Võ <NhanVTSE130478@fpt.edu.vn>
2020-06-14 15:11:52 +0700

    Update notification to db script

--- MSSQL/import-data.sh
+++ MSSQL/import-data.sh
@@ -1,4 +1,4 @@
 # wait for the SQL Server to come up
-sleep 90s
+sleep 20s
 #run the setup script to create the DB and the schema in the DB
 /opt/mssql-tools/bin/sqlcmd -S 127.0.0.1 -U SA -P ${SA_PASSWORD} -i /var/opt/mssql/setup.sql
