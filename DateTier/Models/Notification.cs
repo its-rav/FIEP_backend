9e19a7215dc4c2e65f7a83e1b30ccae169966663
Nhân Võ <NhanVTSE130478@fpt.edu.vn>
2020-06-14 15:11:01 +0700

    Add Notification Model

---
+++ DateTier/Models/Notification.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataTier.Models
+{
+    public partial class Notification
+    {
+        public Guid NotificationID { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string ImageUrl { get; set; }
+        public string? UserFCMTokens { get; set; }
+        public int? EventId { get; set; }
+        public virtual Event? Event { get; set; }
+        public int? GroupId { get; set; }
+        public virtual GroupInformation? GroupInformation { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public DateTime? ModifyDate { get; set; }
+
+    }
+}
