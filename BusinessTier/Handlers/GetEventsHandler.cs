f9f079822023df070437f5e6c24ad2ecadfd2daf
tuấn nguyễn <anhhtuann266@gmail.com>
2020-06-17 13:29:02 +0700

    Add more file

---
+++ BusinessTier/Handlers/GetEventsHandler.cs
@@ -0,0 +1,18 @@
+﻿using BusinessTier.Request;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BusinessTier.Handlers
+{
+    class GetEventsHandler //: IRequestHandler<GetEventsRequest>
+    {
+        //public Task<Unit> Handle(GetEventsRequest request, CancellationToken cancellationToken)
+        //{
+        //    return Task.FromResult();
+        //}
+    }
+}
