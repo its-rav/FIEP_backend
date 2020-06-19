5b4f5891e6542a82ba500406e84a5cd778167621
Nhân Võ <NhanVTSE130478@fpt.edu.vn>
2020-06-19 11:08:17 +0700

    Add event and group statistic DTO

---
+++ BusinessTier/DTO/EventStatisticDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessTier.DTO
+{
+    public class EventStatisticDTO
+    {
+
+        public int EventID { get; set; }
+
+        public string EventName { get; set; }
+
+        public int Followers { get; set; }
+
+        public int postCount { get; set; }
+
+    }
+}
