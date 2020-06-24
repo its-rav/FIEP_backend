a16cbf522deff54cf7508902ce6385161e222750
Nhân Võ <NhanVTSE130478@fpt.edu.vn>
2020-06-25 01:29:26 +0700

    Fix duplicated request properties

---
+++ BusinessTier/Request/GetEventsOfGroupRequest.cs
@@ -0,0 +1,70 @@
+﻿using BusinessTier.Fields;
+using BusinessTier.Response;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessTier.Request
+{
+   public  class GetEventsOfGroupRequest:IRequest<ResponseBase>
+	{
+		private int _GroupId;
+		public int GetGroupId()
+        {
+			return _GroupId;
+        }
+		public void SetGroupId(int GroupId)
+        {
+			this._GroupId = GroupId;
+        }
+		//Search param
+		public string Query { get; set; } = "";
+		//Filter param
+		private int _ApproveState = 1;
+		public Boolean IsUpComming { get; set; } = false;
+		public int ApproveState
+		{
+			get
+			{
+				return _ApproveState;
+			}
+			set
+			{
+				_ApproveState = (value >= -1 && value <= 2) ? value : 1;
+			}
+		}
+		//Paging param
+		const int maxPageSize = 10;
+
+		private int _pageSize = 5;
+		public int PageSize
+		{
+			get
+			{
+				return _pageSize;
+			}
+			set
+			{
+				_pageSize = (value > maxPageSize) ? maxPageSize : value;
+			}
+		}
+		public int PageNumber { get; set; } = 1;
+		//sort param
+		public EventFields Field { get; set; } = 0;
+		public Boolean isDesc { get; set; } = true;
+		//field param
+		private string _FieldSize = "long";
+		public string FieldSize
+		{
+			get
+			{
+				return _FieldSize;
+			}
+			set
+			{
+				_FieldSize = (value.CompareTo("short") == 0 || value.CompareTo("medium") == 0) ? value : "long";
+			}
+		}
+	}
+}
