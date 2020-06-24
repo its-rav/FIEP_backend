2d1184e7eabcfd604706a9603d4a0d66e8963253
tuấn nguyễn <anhhtuann266@gmail.com>
2020-06-24 21:01:28 +0700

    ChangeNotiRequestAndAuthRequest

--- BusinessTier/Request/CreateGroupNotificationRequest.cs
+++ BusinessTier/Request/CreateGroupNotificationRequest.cs
@@ -7,10 +7,9 @@ using System.Text;
 
 namespace BusinessTier.Request
 {
-    public class CreateNotificationRequest : IRequest<ResponseBase>
+    public class CreateGroupNotificationRequest : IRequest<ResponseBase>
     {
         public int? GroupId { get; set; } = null;
-        public int? EventId { get; set; } = null;
         public string Title { get; set; }
         public string Body { get; set; }
         public string ImageUrl { get; set; }
