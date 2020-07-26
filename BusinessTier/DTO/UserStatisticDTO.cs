10f019d1af2d868490776f0109e024f7c8f65c98
Nhân Võ <NhanVTSE130478@fpt.edu.vn>
2020-07-26 17:25:59 +0700

    User statisticDTO

---
+++ BusinessTier/DTO/UserStatisticDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace BusinessTier.DTO
+{
+    class UserStatisticDTO
+    {
+        public Guid UserId { get; set; }
+
+        public DateTime? CreateDate { get; set; }
+
+        public static PropertyInfo[] GetAllProperties()
+        {
+            PropertyInfo[] properties = typeof(UserStatisticDTO).GetProperties();
+            return properties;
+        }
+        public static object GetPropValue(object src, string propName)
+        {
+            return src.GetType().GetProperty(propName).GetValue(src, null);
+        }
+    }
+}
